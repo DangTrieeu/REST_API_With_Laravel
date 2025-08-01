1b2d3747326ac2b86c73361438d71690bb1744ba
DangTrieeu <dangtrieu.lb@gmail.com>
2025-08-01 16:19:16 +0700

    add customer

--- app/Http/Requests/StoreCustomerRequest.php
+++ app/Http/Requests/StoreCustomerRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreCustomerRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreCustomerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,13 @@ class StoreCustomerRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required'],
+            'type' => ['required', Rule::in(['I', 'B'])],
+            'email' => ['required', 'email'],
+            'address' => ['required'],
+            'city' => ['required'],
+            'state' => ['required'],
+            'postal_code' => ['required'],
         ];
     }
 }
